7165aad9757e548ef036b541ed89b9330dc67347
Jack Kinsey <jack.kinsey@technivation.com>
2019-05-08 15:43:42 -0500

    Adds new IDP project and sets up and configures IdentityServer4

---
+++ src/Marvin.IDP/Config.cs
@@ -0,0 +1,58 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        // Test Users
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "3e316ac5-8354-400c-8663-dc514b7fa94a",
+                    Username = "Frank",
+                    Password = "password",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "521ecf9c-9e65-4b5c-96dd-4bba30ce936c",
+                    Username = "Claire",
+                    Password = "Franksux",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood")
+                    }
+                }
+            };
+        }
+
+        // Identity-related resources (scopes)
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
